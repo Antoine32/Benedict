8299faef8ca090b4b5720aef5a3ecc3edc8df149
Antoine32 <Antoine32@github.com>
2020-11-11 15:53:23 +0000

    a

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 cd /home/pi/Benedict
+
 git pull
-screen -R -S Benedict -m node botB.js
+
+tmux \
+  new-session -d -s Benedict "node botB.js ; read" \; \
+  select-layout even-vertical
+
+sleep 30s
+
+tmux \
+  new-session -d -s Sensor "python3 sensor.py ; read" \; \
+  select-layout even-vertical
