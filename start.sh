46d03fc16c481ba8dff2454866a069e0204ce01c
Antoine32 <Antoine32@github.com>
2020-11-11 15:56:08 +0000

    a

--- start.sh
+++ start.sh
@@ -6,8 +6,11 @@ tmux \
   new-session -d -s Benedict "node botB.js ; read" \; \
   select-layout even-vertical
 
+echo "Bot running"
 sleep 30s
 
 tmux \
   new-session -d -s Sensor "python3 sensor.py ; read" \; \
   select-layout even-vertical
+
+echo "Sensor scanning"
